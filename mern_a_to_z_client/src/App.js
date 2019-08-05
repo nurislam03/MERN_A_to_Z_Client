85212e5aba4cfd35dcc4d22acb67b8e0328806f1
Nur Islam <nurislam0333@gmail.com>
2019-08-05 07:40:38 +0600

    done

--- mern_a_to_z_client/src/App.js
+++ mern_a_to_z_client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route path='/create-book' component={CreateBook} />
+          <Route path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ mern_a_to_z_client/src/components/UpdateBookInfo.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import '../App.css';
+
+class UpdateBookInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: {}
+    };
+  }
+
+  componentDidMount() {
+    // console.log("Print id: " + this.props.match.params.id);
+    axios
+      .get('http://localhost:8082/api/books/'+this.props.match.params.id)
+      .then(res => {
+        this.setState({
+          book: res.data
+        })
+      })
+      .catch(err => {
+        console.log("Error from UpdateBookInfo");
+      })
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const data = {
+      title: this.state.title,
+      isbn: this.state.isbn,
+      author: this.state.author,
+      description: this.state.description,
+      published_date: this.state.published_date,
+      publisher: this.state.publisher
+    };
+
+    axios
+      .put('http://localhost:8082/api/books/'+this.props.match.params.id, data)
+      .then(res => {
+        this.props.history.push('/show-book/'+this.props.match.params.id);
+      })
+      .catch(err => {
+        console.log("Error in UpdateBookInfo!");
+      })
+  };
+
+
+  render() {
+    return (
+      <div className="UpdateBookInfo">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show BooK List
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Edit Book</h1>
+              <p className="lead text-center">
+                  Update Book's Info
+              </p>
+            </div>
+          </div>
+
+          <div>
+          <form noValidate onSubmit={this.onSubmit}>
+            <div className='form-group'>
+              <label htmlFor="title">Title</label>
+              <input
+                type='text'
+                placeholder='Title of the Book'
+                name='title'
+                className='form-control'
+                value={this.state.book.title}
+                onChange={this.onChange}
+              />
+            </div>
+            <br />
+
+            <div className='form-group'>
+            <label htmlFor="isbn">ISBN</label>
+              <input
+                type='text'
+                placeholder='ISBN'
+                name='isbn'
+                className='form-control'
+                value={this.state.book.isbn}
+                onChange={this.onChange}
+              />
+            </div>
+
+            <div className='form-group'>
+            <label htmlFor="author">Author</label>
+              <input
+                type='text'
+                placeholder='Author'
+                name='author'
+                className='form-control'
+                value={this.state.book.author}
+                onChange={this.onChange}
+              />
+            </div>
+
+            <div className='form-group'>
+            <label htmlFor="description">Description</label>
+              <input
+                type='text'
+                placeholder='Describe this book'
+                name='description'
+                className='form-control'
+                value={this.state.book.description}
+                onChange={this.onChange}
+              />
+            </div>
+
+            {/* <div className='form-group'>
+            <label htmlFor="published_date">Published Date</label>
+              <input
+                type='date'
+                placeholder='published_date'
+                name='published_date'
+                className='form-control'
+                value={this.state.book.published_date}
+                onChange={this.onChange}
+              />
+            </div> */}
+            <div className='form-group'>
+            <label htmlFor="publisher">Publisher</label>
+              <input
+                type='text'
+                placeholder='Publisher of this Book'
+                name='publisher'
+                className='form-control'
+                value={this.state.book.publisher}
+                onChange={this.onChange}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-outline-info btn-lg btn-block">Update Book</button>
+            </form>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UpdateBookInfo;
