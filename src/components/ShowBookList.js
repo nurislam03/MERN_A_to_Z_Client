4a4a12467bc317bb15fa46b7e45c5e7af9ba16fb
Abhinav-26 <abhinavdubey616@gmail.com>
2022-09-06 15:54:35 +0530

    added backend urls

--- src/components/ShowBookList.js
+++ src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get('http://localhost:8082/api/books')
+      .get('http://mern-backend-devtron-demo-service.devtron-demo:8082/api/books')
       .then(res => {
         this.setState({
           books: res.data
--- src/components/UpdateBookInfo.js
+++ src/components/UpdateBookInfo.js
@@ -19,7 +19,7 @@ class UpdateBookInfo extends Component {
   componentDidMount() {
     // console.log("Print id: " + this.props.match.params.id);
     axios
-      .get('http://localhost:8082/api/books/'+this.props.match.params.id)
+      .get('http://mern-backend-devtron-demo-service.devtron-demo:8082/api/books/'+this.props.match.params.id)
       .then(res => {
         // this.setState({...this.state, book: res.data})
         this.setState({
@@ -53,7 +53,7 @@ class UpdateBookInfo extends Component {
     };
 
     axios
-      .put('http://localhost:8082/api/books/'+this.props.match.params.id, data)
+      .put('http://mern-backend-devtron-demo-service.devtron-demo:8082/api/books/'+this.props.match.params.id, data)
       .then(res => {
         this.props.history.push('/show-book/'+this.props.match.params.id);
       })
